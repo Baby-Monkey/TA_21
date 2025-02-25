8bf834af0054f84bd3691818b0071ba1e6fe75f9
Baby-Monkey <104055635+Baby-Monkey@users.noreply.github.com>
2025-02-25 09:26:43 +0200

    Add files via upload

--- saptamana_2/sign_in_tests.py
+++ saptamana_2/sign_in_tests.py
@@ -12,14 +12,15 @@ class SignIn (unittest.TestCase):
     EMAIL_INPUT = (By.ID, "email")
     PASSWORD_INPUT = (By.XPATH, "//input[ @ title = 'Password']")
     SUBMIT_BUTTON = (By.XPATH, "//button[@class='action login primary']")
-    UNEXISTING_ERROR_MESSAGE = (By.XPATH, '//div[@data-bind="html: $parent.prepareMessageForHtml(message.text)"]')
+    # UNEXISTING_ERROR_MESSAGE = (By.XPATH, '//div[@data-bind="html: $parent.prepareMessageForHtml(message.text)"]')
+    UNEXISTING_ERROR_MESSAGE = (By.CLASS_NAME, "message-error")
     INVALID_ERROR_MESSAGE = (By.ID, 'email-error')
     EMPTY_EMAIL_ERROR_MESSAGE = (By.ID, 'email-error')
     EMPTY_PAS_ERROR_MESSAGE = (By.ID, 'pass-error')
 
 
     def setUp(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(10)
         self.driver.get('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/')
         self.driver.maximize_window()
@@ -42,7 +43,7 @@ class SignIn (unittest.TestCase):
         self.insert_login_data("unexisting_email@gamil.com", "unexisting_pass")
         # time.sleep(3)
         expected_error_message = "The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later."
-        WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(self.UNEXISTING_ERROR_MESSAGE))
+        # WebDriverWait(self.driver, 10).until(expected_conditions.visibility_of_element_located(self.UNEXISTING_ERROR_MESSAGE))
         # actual_error_message = self.driver.find_element(By.XPATH, '//div[@data-bind="html: $parent.prepareMessageForHtml(message.text)"]').text
         # self.assertEqual(expected_error_message, actual_error_message, "Error message is different")
         self.evaluate_error_message(expected_error_message, self.UNEXISTING_ERROR_MESSAGE)
@@ -56,8 +57,9 @@ class SignIn (unittest.TestCase):
         time.sleep(1)
         expected_error_message = "Please enter a valid email address (Ex: johndoe@domain.com)."
         # actual_error_message = self.driver.find_element(By.ID, 'email-error').text
+
         # self.assertEqual(expected_error_message, actual_error_message, "Error message is different")
-        self.evaluate_error_message(expected_error_message, self.INVALID_ERROR_MESSAGE)
+        self.evaluate_error_message(expected_error_message, self.INVALID_ERROR_MESSAGE.text)
 
 
     def test_login_with_empty_credentials(self):
