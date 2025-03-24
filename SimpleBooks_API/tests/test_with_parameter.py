1ded2a13f6674240e0338439e38b4b65ec3f010e
Baby-Monkey <104055635+Baby-Monkey@users.noreply.github.com>
2025-03-24 20:27:52 +0200

    Update test_with_parameter.py

--- SimpleBooks_API/tests/test_with_parameter.py
+++ SimpleBooks_API/tests/test_with_parameter.py
@@ -1,15 +1,14 @@
-import unittest
-
 import pytest
 
+from api_requests.simple_books_requests import SimpleBooksRequests
 
-class TestDebug(unittest.TestCase):
 
-    @pytest.mark.skip
-    @pytest.mark.parametrize("book_type", [11, "comedy"],)
+class TestDebug():
+
+    @pytest.mark.debugparam
+    @pytest.mark.parametrize("book_type", [11, "comedy", True])
     def test_get_list_of_books_filter_by_invalid_type(self, book_type):
-        print("book_type = ", book_type)
-        # response = SimpleBooksRequests.get_list_of_books(type=book_type)
-        # expected_error_message = "Invalid value for query parameter 'type'. Must be one of: fiction, non-fiction."
-        # assert response.status_code == 400, "Unexpected status code"
-        # assert response.json()["error"] == expected_error_message, "Unexpected error message"
+        response = SimpleBooksRequests.get_list_of_books(type=book_type)
+        expected_error_message = "Invalid value for query parameter 'type'. Must be one of: fiction, non-fiction."
+        assert response.status_code == 400, "Unexpected status code"
+        assert response.json()["error"] == expected_error_message, "Unexpected error message"
